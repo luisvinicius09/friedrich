fcf5e0bf6a93a36b74ca7d90bb81c843c5d61368
Luis V <luisvinicius0906@gmail.com>
2023-08-23 17:07:34 -0300

    Add: jwt logic, with dependencies, middleware and typing

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import fastify, { FastifyInstance } from 'fastify';
+import fastifyJwt from '@fastify/jwt';
 
 import { appRoutes } from '@/http/routes';
 import { ZodError } from 'zod';
 import { env } from './env';
 
 export const app: FastifyInstance = fastify({});
+
+app.register(fastifyJwt, { secret: env.JWT_SECRET });
 
 app.register(appRoutes);
 
