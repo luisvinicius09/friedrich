d4856bb4a9817f391a4bdf2e5d4b5eb7a21527f8
Luis V <luisvinicius0906@gmail.com>
2023-08-28 16:48:20 -0300

    Add: users addresses repository interface

---
+++ src/repositories/interfaces/users-addresses-repository.ts
@@ -0,0 +1,12 @@
+import { Prisma, UserAddress } from '@prisma/client';
+
+export interface UsersAddressesRepository {
+	create: (data: Prisma.UserAddressUncheckedCreateInput) => Promise<UserAddress>;
+
+	findByUserId: (userId: string) => Promise<UserAddress | null>;
+
+	update: (
+		userId: string,
+		data: Prisma.UserAddressUpdateWithoutUserInput,
+	) => Promise<UserAddress>;
+}
