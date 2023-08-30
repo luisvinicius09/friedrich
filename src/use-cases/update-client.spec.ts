cc10c82827a6a2ffb34328021785278602e1e253
Luis V <luisvinicius0906@gmail.com>
2023-08-30 16:36:21 -0300

    Add: use case to update client and test

---
+++ src/use-cases/update-client.spec.ts
@@ -0,0 +1,51 @@
+import { InMemoryUsersClientsRepository } from '@/repositories/in-memory/in-memory-users-clients-repository';
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { UsersRepository } from '@/repositories/interfaces/users-repository';
+import { UsersClientsRepository } from '@/repositories/interfaces/users-clients-repository';
+import { UpdateClientUseCase } from './update-client';
+
+let usersRepository: UsersRepository;
+let clientsRepository: UsersClientsRepository;
+let sut: UpdateClientUseCase;
+
+describe('Update client use case', () => {
+	beforeEach(() => {
+		usersRepository = new InMemoryUsersRepository();
+		clientsRepository = new InMemoryUsersClientsRepository();
+		sut = new UpdateClientUseCase(clientsRepository);
+	});
+
+	it('should update client correctly', async () => {
+		const user = await usersRepository.create({
+			name: 'John Doe',
+			email: 'joe-doe@email.com',
+			document: '123456789',
+			password: 'joe-doe-pw',
+			phoneNumber: 123456789,
+		});
+
+		const clientCreated = await clientsRepository.create({
+			document: '123456789',
+			name: 'John Doe',
+			phoneNumber: 123456789,
+			userId: user.id,
+		});
+
+		expect(clientCreated.name).toEqual('John Doe');
+		expect(clientCreated.phoneNumber).toEqual(123456789);
+		expect(clientCreated.document).toEqual('123456789');
+
+		const { userClient: client } = await sut.execute({
+			document: '132345',
+			name: 'John Doe 2',
+			phoneNumber: 12345,
+			clientId: clientCreated.id,
+		});
+
+		expect(client.id).toEqual(expect.any(String));
+		expect(client.name).toEqual('John Doe 2');
+		expect(client.phoneNumber).toEqual(12345);
+		expect(client.document).toEqual('132345');
+	});
+});
---
+++ src/use-cases/update-client.ts
@@ -0,0 +1,38 @@
+import { UsersClientsRepository } from '@/repositories/interfaces/users-clients-repository';
+import { UserClient } from '@prisma/client';
+
+interface UpdateClientUseCaseRequest {
+	clientId: string;
+	name: string;
+	document: string;
+	phoneNumber: number;
+}
+
+interface UpdateClientUseCaseResponse {
+	userClient: UserClient;
+}
+
+export class UpdateClientUseCase {
+	constructor(private usersClientsRepository: UsersClientsRepository) {}
+
+	async execute({
+		clientId,
+		name,
+		document,
+		phoneNumber,
+	}: UpdateClientUseCaseRequest): Promise<UpdateClientUseCaseResponse> {
+		const client = await this.usersClientsRepository.findByClientId(clientId);
+
+		if (!client) {
+			throw new Error('Client not found');
+		}
+
+		const userClient = await this.usersClientsRepository.update(clientId, {
+			name: name,
+			document: document,
+			phoneNumber: phoneNumber,
+		});
+
+		return { userClient };
+	}
+}
